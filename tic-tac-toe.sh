2b951f93a87e80e16590ca17eafce414d8556a8e
Kartikey <parasharkartikey@gmail.com>
2020-02-24 12:42:54 +0530

    Added UC8 branch

--- tic-tac-toe.sh
+++ tic-tac-toe.sh
@@ -134,53 +134,138 @@ function checkWin()
 
 function computer_win_check()
 {
+	block=0
 	for (( num=1; num<=3; num++ ))
 	do
-		for (( num_in=1; num_in<=3; num_in++ ))
+		for (( j=1; j<=3; j++ ))
 		do
 #........Horizontal...........................................................................................................................
-			if [[ ${board[$i,$j]} == "-" && ${board[$i,$(( $j + 1 ))]} == $computer && {board[$i,$(( $j + 2 ))]} == $computer ]]
+			if [[ ${board[$num,$j]} == "-" && ${board[$num,$(( $j + 1 ))]} == $computer && {board[$num,$(( $j + 2 ))]} == $computer ]]
 			then
-				board[$i,$j]=$computer
-			elif [[ ${board[$i,$j]} == $computer && ${board[$i,$(( $j + 1 ))]} == "-" && {board[$i,$(( $j + 2 ))]} == $computer ]]
+				board[$num,$j]=$computer
+				block=1
+			elif [[ ${board[$num,$j]} == $computer && ${board[$num,$(( $j + 1 ))]} == "-" && {board[$num,$(( $j + 2 ))]} == $computer ]]
 			then
-				board[$i,$(( $j + 1 ))]=$computer
-			elif [[ ${board[$i,$j]} == $computer && ${board[$i,$(( $j + 1 ))]} == $computer && {board[$i,$(( $j + 2 ))]} == "-" ]]
+				board[$num,$(( $j + 1 ))]=$computer
+				block=1
+			elif [[ ${board[$num,$j]} == $computer && ${board[$num,$(( $j + 1 ))]} == $computer && {board[$num,$(( $j + 2 ))]} == "-" ]]
 			then
-				board[$i,$(( $j + 2 ))]=$computer
+				board[$num,$(( $j + 2 ))]=$computer
+				block=1
 			fi
 #.......Vertical..............................................................................................................................
-			if [[ ${board[$j,$i]} == "-" && ${board[$(( $j + 1 )),$i]} == $computer && ${board[$(( $j + 2 )),$i]} == $computer ]]
+			if [[ ${board[$j,$num]} == "-" && ${board[$(( $j + 1 )),$num]} == $computer && ${board[$(( $j + 2 )),$num]} == $computer ]]
 			then
-				board[$j,$i]=$computer
-			elif [[ ${board[$j,$i]} == $computer && ${board[$(( $j + 1 )),$i]} == "-" && ${board[$(( $j + 2 )),$i]} == $computer ]]
+				board[$j,$num]=$computer
+				block=1
+			elif [[ ${board[$j,$num]} == $computer && ${board[$(( $j + 1 )),$num]} == "-" && ${board[$(( $j + 2 )),$num]} == $computer ]]
 			then
-				board[$(( $j + 1 )),$i]=$computer
-			elif [[ ${board[$j,$i]} == $computer && ${board[$(( $j + 1 )),$i]} == $computer && ${board[$(( $j + 2 )),$i]} == "-" ]]
+				board[$(( $j + 1 )),$num]=$computer
+				block=1
+			elif [[ ${board[$j,$num]} == $computer && ${board[$(( $j + 1 )),$num]} == $computer && ${board[$(( $j + 2 )),$num]} == "-" ]]
 			then
-				board[$(( $j + 2 )),$i]=$computer
+				board[$(( $j + 2 )),$num]=$computer
+				block=1
 			fi
 #........Diagonal1.............................................................................................................................
-			if [[ ${board[$i,$j]} == "-" && ${board[$(( $i + 1 )),$(( $j + 1 ))]} == $computer && ${board[$(( $i + 2 )),$(( $j + 2 ))]} == $computer ]]
+			if [[ ${board[$num,$j]} == "-" && ${board[$(( $num + 1 )),$(( $j + 1 ))]} == $computer && ${board[$(( $num + 2 )),$(( $j + 2 ))]} == $computer ]]
 			then
-				board[$i,$j]=$computer
-			elif [[ ${board[$i,$j]} == $computer && ${board[$(( $i + 1 )),$(( $j + 1 ))]} == "-" && ${board[$(( $i + 2 )),$(( $j + 2 ))]} == $computer ]]
+				board[$num,$j]=$computer
+				block=1
+			elif [[ ${board[$num,$j]} == $computer && ${board[$(( $num + 1 )),$(( $j + 1 ))]} == "-" && ${board[$(( $num + 2 )),$(( $j + 2 ))]} == $computer ]]
 			then
-				board[$(( $i + 1 )),$(( $j + 1 ))]}=$computer
-			elif [[ ${board[$i,$j]} == $computer && ${board[$(( $i + 1 )),$(( $j + 1 ))]} == $computer && ${board[$(( $i + 2 )),$(( $j + 2 ))]} == "-" ]]
+				board[$(( $num + 1 )),$(( $j + 1 ))]}=$computer
+				block=1
+			elif [[ ${board[$num,$j]} == $computer && ${board[$(( $num + 1 )),$(( $j + 1 ))]} == $computer && ${board[$(( $num + 2 )),$(( $j + 2 ))]} == "-" ]]
 			then
-				board[$(( $i + 2 )),$(( $j + 2 ))]=$computer
+				board[$(( $num + 2 )),$(( $j + 2 ))]=$computer
+				block=1
 			fi
 #........Diagonal2.............................................................................................................................
-			if [[ ${board[$i,$(( $j + 2 ))]} == "-" && ${board[$(( $i + 1 )),$(( $j + 1 ))]} == $computer && ${board[$(( $i + 2 )),$j]} == $computer ]]
+			if [[ ${board[$num,$(( $j + 2 ))]} == "-" && ${board[$(( $num + 1 )),$(( $j + 1 ))]} == $computer && ${board[$(( $num + 2 )),$j]} == $computer ]]
 			then
-				board[$i,$j]=$computer
-			elif [[ ${board[$i,$(( $j + 2 ))]} == $computer && ${board[$(( $i + 1 )),$(( $j + 1 ))]} == "-" && ${board[$(( $i + 2 )),$j]} == $computer ]]
+				board[$num,$j]=$computer
+				block=1
+			elif [[ ${board[$num,$(( $j + 2 ))]} == $computer && ${board[$(( $num + 1 )),$(( $j + 1 ))]} == "-" && ${board[$(( $num + 2 )),$j]} == $computer ]]
 			then
-				board[$(( $i + 1 )),$(( $j + 1 ))]=$computer
-			elif [[ ${board[$i,$(( $j + 2 ))]} == $computer && ${board[$(( $i + 1 )),$(( $j + 1 ))]} == $computer && ${board[$(( $i + 2 )),$j]} == "-" ]]
+				board[$(( $num + 1 )),$(( $j + 1 ))]=$computer
+				block=1
+			elif [[ ${board[$num,$(( $j + 2 ))]} == $computer && ${board[$(( $num + 1 )),$(( $j + 1 ))]} == $computer && ${board[$(( $num + 2 )),$j]} == "-" ]]
 			then
-				board[$i,$j]=$computer
+				board[$num,$j]=$computer
+				block=1
+			fi
+		done
+	done
+	if [ $block -eq 1 ]
+	then
+		display_board
+		checkWin
+	fi
+}
+
+
+function computer_block_check()
+{
+	for (( num=1; num<=3; num++ ))
+	do
+		for (( j=1; j<=3; j++ ))
+		do
+#........Horizontal...........................................................................................................................
+			if [[ ${board[$num,$j]} == "-" && ${board[$num,$(( $j + 1 ))]} == $player && {board[$num,$(( $j + 2 ))]} == $player ]]
+			then
+				board[$num,$j]=$computer
+				block=1
+			elif [[ ${board[$num,$j]} == $player && ${board[$num,$(( $j + 1 ))]} == "-" && {board[$num,$(( $j + 2 ))]} == $player ]]
+			then
+				board[$num,$(( $j + 1 ))]=$computer
+				block=1
+			elif [[ ${board[$num,$j]} == $player && ${board[$num,$(( $j + 1 ))]} == $player && {board[$num,$(( $j + 2 ))]} == "-" ]]
+			then
+				board[$num,$(( $j + 2 ))]=$computer
+				block=1
+			fi
+#.......Vertical..............................................................................................................................
+			if [[ ${board[$j,$num]} == "-" && ${board[$(( $j + 1 )),$num]} == $player && ${board[$(( $j + 2 )),$num]} == $player ]]
+			then
+				board[$j,$num]=$computer
+				block=1
+			elif [[ ${board[$j,$num]} == $player && ${board[$(( $j + 1 )),$num]} == "-" && ${board[$(( $j + 2 )),$num]} == $player ]]
+			then
+				board[$(( $j + 1 )),$num]=$computer
+				block=1
+			elif [[ ${board[$j,$num]} == $player && ${board[$(( $j + 1 )),$num]} == $player && ${board[$(( $j + 2 )),$num]} == "-" ]]
+			then
+				board[$(( $j + 2 )),$num]=$computer
+				block=1
+			fi
+#........Diagonal1.............................................................................................................................
+			if [[ ${board[$num,$j]} == "-" && ${board[$(( $num + 1 )),$(( $j + 1 ))]} == $player && ${board[$(( $num + 2 )),$(( $j + 2 ))]} == $player ]]
+			then
+				board[$num,$j]=$computer
+				block=1
+			elif [[ ${board[$num,$j]} == $player && ${board[$(( $num + 1 )),$(( $j + 1 ))]} == "-" && ${board[$(( $num + 2 )),$(( $j + 2 ))]} == $player ]]
+			then
+				board[$(( $num + 1 )),$(( $j + 1 ))]}=$computer
+				block=1
+			elif [[ ${board[$num,$j]} == $player && ${board[$(( $num + 1 )),$(( $j + 1 ))]} == $player && ${board[$(( $num + 2 )),$(( $j + 2 ))]} == "-" ]]
+			then
+				board[$(( $num + 2 )),$(( $j + 2 ))]=$computer
+				block=1
+			fi
+#........Diagonal2.............................................................................................................................
+			if [[ ${board[$num,$(( $j + 2 ))]} == "-" && ${board[$(( $num + 1 )),$(( $j + 1 ))]} == $player && ${board[$(( $num + 2 )),$j]} == $player ]]
+			then
+				board[$num,$j]=$computer
+				block=1
+			elif [[ ${board[$num,$(( $j + 2 ))]} == $player && ${board[$(( $num + 1 )),$(( $j + 1 ))]} == "-" && ${board[$(( $num + 2 )),$j]} == $player ]]
+			then
+				board[$(( $num + 1 )),$(( $j + 1 ))]=$computer
+				block=1
+			elif [[ ${board[$num,$(( $j + 2 ))]} == $player && ${board[$(( $num + 1 )),$(( $j + 1 ))]} == $player && ${board[$(( $num + 2 )),$j]} == "-" ]]
+			then
+				board[$num,$j]=$computer
+				block=1
 			fi
 		done
 	done
@@ -203,37 +288,45 @@ function player_turn()
 	if [[ ${board[$row,$column]} == "-" ]]
 	then
 		board[$row,$column]=$player
-		((movecount++))
-		display_board
-		echo "@ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $"
 		checkWin
-		computer_turn
+
 	else
 		echo "Position Occupied or Invalid Position for user"
 		tie_game
 		player_turn
 	fi
+	((movecount++))
+	display_board
+	echo "@ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $ @ $"
+	tie_game
+	computer_turn
 }
 
 function computer_turn()
 {
 	echo "--------------------------------------------------------COMPUTER TURN-----------------------------------------------------------------"
-	row=$(( ( RANDOM % 3 ) + 1 ))
-	column=$(( ( RANDOM % 3 ) + 1 ))
-		if [[ ${board[$row,$column]} == "-" ]]
-		then
-			board[$row,$column]=$computer
-			((movecount++))
-			display_board
-			checkWin
-			computer_win_check
-			
-			player_turn
-		else
-			echo "Position Occupied"
-			tie_game
-			computer_turn
-		fi
+	checkWin
+	computer_win_check
+	if [ $block -eq 0 ]
+	then
+		computer_block_check
+	fi
+	if [ $block -eq 0 ]
+	then
+		row=$(( ( RANDOM % 3 ) + 1 ))
+		column=$(( ( RANDOM % 3 ) + 1 ))
+			if [[ ${board[$row,$column]} == "-" ]]
+			then
+				board[$row,$column]=$computer
+			else
+				echo "Position Occupied"
+				computer_turn
+			fi
+	fi
+	((movecount++))
+	display_board
+	tie_game
+	player_turn
 }
 
 assign_symbol
